7e0de9316f2a7d89565365f01caff26e7053005f
Shrey Jalota <shreyjalota@Mac.lan>
2026-06-06 23:18:38 +0100

    “Ready“

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/dm-sans/300.css";
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+import "@fontsource/dm-sans/700.css";
+import "@fontsource/dm-serif-display/400.css";
+import "@fontsource/dm-serif-display/400-italic.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
